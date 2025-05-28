b7760eeebf837b98d2537f3a27bd5cc779c41aab
adam-dev2 <shaikadam252@gmail.com>
2025-05-28 19:55:54 +0530

    updated with the changes verication needed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './Layout'
 import Dashboard from './components/Dashboard'
 import Signin from './pages/Signin'
 import Login from './pages/Login'
+import Welcome from './components/Welcome'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path='/' element={<Signin />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Layout />}>
+          <Route index element={<Welcome />} />
           <Route path=':projectId' element={<Dashboard />} />
         </Route>
       </Routes>
